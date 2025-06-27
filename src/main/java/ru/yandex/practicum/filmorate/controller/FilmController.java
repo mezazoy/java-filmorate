7cc73545a58fadb3ba2262a3519aec6f3845b423
Konstantin <mezazoy1@yandex.ru>
2025-06-27 17:10:48 +0300

    добавить логирование в классы FilmController и UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -12,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/films")
+@Slf4j
 public class FilmController {
     private Map<Integer, Film> films = new HashMap<>();
     private final LocalDate movieBirthday = LocalDate.parse("28.12.1985");
@@ -25,51 +27,64 @@ public class FilmController {
     @PostMapping
     public Film create(@RequestBody Film film) {
         if (film.getName() == null || film.getName().isBlank()) {
+            log.warn("Ошибка при добавлении фильма, не указано название");
             throw new ValidationException("У фильма должно быть название!");
         }
 
         if (film.getDescription().length() > MAXLENGTHDESCR) {
+            log.warn("Ошибка при добавлении фильма, слишком длинное описание");
             throw new ValidationException("Длина описания больше максимально допустимой(200 символов)");
         }
 
         if (film.getReleaseDate().isBefore(movieBirthday)) {
+            log.warn("Ошибка при добавлении фильма, дата релиза = {}", film.getReleaseDate());
             throw new ValidationException("Дата релиза фильма не может быть раньше " + movieBirthday);
         }
 
         if (!durationCheck(film.getDuration())) {
+            log.warn("Ошибка при добавлении фильма, длительность = {}", film.getDuration());
             throw new ValidationException("Длительность должна быть больше 0");
         }
 
         film.setId(getNextId());
+        log.trace("Установлен id фильма");
         films.put(film.getId(), film);
+        log.info("Фильм добавлен");
         return film;
     }
 
     @PutMapping
     public Film update(@RequestBody Film newFilm) {
         if (newFilm.getId() == null) {
+            log.warn("Ошибка при обновлении данных фильма, не указан id");
             throw new ValidationException("id должен быть указан!");
         }
 
         if (films.containsKey(newFilm.getId())) {
+            log.trace("фильм с id = {} найден", newFilm.getId());
             Film oldFilm = films.get(newFilm.getId());
             if (newFilm.getName() != null) {
                 oldFilm.setName(newFilm.getName());
+                log.trace("Обновлено название фильма");
             }
 
             if (newFilm.getDescription() != null && !(newFilm.getDescription().isBlank())) {
                 oldFilm.setDescription(newFilm.getDescription());
+                log.trace("Обновлено описание филма");
             }
 
             if (newFilm.getReleaseDate() != null) {
                 oldFilm.setReleaseDate(newFilm.getReleaseDate());
+                log.trace("Обновлена дата релтиза фильма");
             }
 
             if (newFilm.getDuration() != null && durationCheck(newFilm.getDuration())) {
                 oldFilm.setDuration(newFilm.getDuration());
+                log.trace("Обновлена длительность фильма");
             }
         }
 
+        log.warn("Ошибка при обновлении данных фильма, указан неверный id");
         throw new ValidationException("Фильм с id = " + newFilm.getId() + " не найден");
     }
 
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
@@ -12,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
     private Map<Integer, User> users = new HashMap<>();
 
@@ -23,53 +25,66 @@ public class UserController {
     @PostMapping
     public User create(@RequestBody User user) {
         if (user.getEmail() == null || user.getEmail().isBlank() || !(validateEmail(user.getEmail()))) {
+            log.warn("Ошибка при создании пользователя, некорректный email");
             throw new ValidationException("Введён не корректный email");
         }
 
         if (user.getLogin() == null || user.getLogin().contains(" ")) {
+            log.warn("Ошибка при создании пользователя, некорректный login");
             throw new ValidationException("Логин не может быть пустым и содержать пробелы!");
         }
 
         if (user.getName() == null || user.getName().isBlank()) {
             user.setName(user.getLogin());
+            log.trace("Установлено имя пользователя по умолчанию");
         }
 
         if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.warn("Ошибка при создании пользователя, некорректная дата рождения");
             throw new ValidationException("Дата рождения не может быть в будущем!");
         }
 
         user.setId(getNextId());
+        log.trace("Установлен id пользователя");
         users.put(user.getId(), user);
+        log.info("Пользователь добавлен");
         return user;
     }
 
     @PutMapping
     public User update(@RequestBody User newUser) {
         if (newUser.getId() == null) {
+            log.warn("Ошибка при обновления данных пользователя, не указан id");
             throw new ValidationException("id должен быть указан!");
         }
 
         if (users.containsKey(newUser.getId())) {
+            log.trace("Пользователь с id = {} найден", newUser.getId());
             User oldUser = users.get(newUser.getId());
             if (newUser.getEmail() != null && !(newUser.getEmail().isBlank()) && validateEmail(newUser.getEmail())) {
                 oldUser.setEmail(newUser.getEmail());
+                log.trace("Обновлён email пользователя");
             }
 
             if (newUser.getLogin() != null && !(newUser.getLogin().isBlank())) {
                 oldUser.setLogin(newUser.getLogin());
+                log.trace("Обновлен логин пользователя");
             }
 
             if (newUser.getName() != null && !(newUser.getName().isBlank())) {
                 oldUser.setName(newUser.getName());
+                log.trace("Обновлено имя пользователя");
             } else {
                 oldUser.setName(newUser.getLogin());
+                log.trace("Имя пользователя утановлено по умолчанию");
             }
-
             if (newUser.getBirthday() != null && !(newUser.getBirthday().isAfter(LocalDate.now()))) {
                 oldUser.setBirthday(newUser.getBirthday());
+                log.trace("День рождения пользователя обновлено");
             }
         }
 
+        log.warn("Ошибка при обновления данных пользователя, указан не верный id");
         throw new ValidationException("Пользователя с id = " + newUser.getId() + " не найден!");
     }
 
